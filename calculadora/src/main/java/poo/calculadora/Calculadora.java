6da0e1af0269459195aee7f2580d68831627c739
PradierLucas <113732218+PradierLucas@users.noreply.github.com>
2024-05-28 21:29:20 -0300

    calculadora

---
+++ calculadora/src/main/java/poo/calculadora/Calculadora.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package poo.calculadora;
+
+import igu.Pantalla;
+
+/**
+ *
+ * @author lucas
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+       
+        Pantalla calc = new Pantalla();
+        calc.setVisible(true);
+        calc.setLocationRelativeTo(null);
+    }
+}
